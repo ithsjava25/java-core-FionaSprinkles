a053af67f00a9a287a069b2e8f0603ddd79b45e5
Fiona Friberg <fionafriberg@outlook.com>
2025-10-17 15:04:04 +0200

    Add initial code for FoodProduct class

--- src/main/java/com/example/FoodProduct.java
+++ src/main/java/com/example/FoodProduct.java
@@ -1,4 +1,41 @@
 package com.example;
 
-public class FoodProduct extends  Product {
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+public class FoodProduct extends Product implements Perishable, Shippable {
+
+
+    private LocalDate expirationDate;
+    BigDecimal weight;
+
+    public FoodProduct(UUID id, String name, Category category, BigDecimal price, LocalDate expirationDate, BigDecimal weight) {
+        super(id, name, category, price);
+        this.expirationDate = expirationDate;
+        this.weight = weight;
+
+        if (price.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException("Price can't be negative");
+        }
+        if (weight.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException("Weight can't be negative");
+        }
+
+    }
+
+    //productDetails() should look like: "Food: Milk, Expires: 2025-12-24".
+    //todo: I guess this is my gettermethod from product.
+    @Override
+    String productDetails() {
+        return "Food: " + name() + ", Expires: " + expirationDate;
+    }
+
+    //Shipping rule: cost = weight * 50.
+    private static BigDecimal shippingRule (BigDecimal weight) {
+        BigDecimal shippingCost = weight.multiply(BigDecimal.valueOf(50));
+        return shippingCost;
+    }
+
 }
+
